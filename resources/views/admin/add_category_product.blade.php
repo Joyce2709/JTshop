65b2d8e25d417154eef70d7483c03057c85edef0
Joyce <77377941+Joyce2709@users.noreply.github.com>
2022-03-05 23:05:12 +0700

    update

--- resources/views/admin/add_category_product.blade.php
+++ resources/views/admin/add_category_product.blade.php
@@ -4,30 +4,29 @@
             <div class="col-lg-12">
                     <section class="panel">
                         <header class="panel-heading">
-                            Basic Forms
+                           Thêm Danh Mục Sản Phẩm
                         </header>
                         <div class="panel-body">
                             <div class="position-center">
                                 <form role="form">
                                 <div class="form-group">
-                                    <label for="exampleInputEmail1">Email address</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                                    <label for="exampleInputEmail1">Tên Danh Mục</label>
+                                    <input type="email" name="category_product_name" class="form-control" 
+                                    id="exampleInputEmail1" placeholder="Tên Danh Mục">
                                 </div>
                                 <div class="form-group">
-                                    <label for="exampleInputPassword1">Password</label>
+                                    <label for="exampleInputPassword1">Mô Tả Danh Mục</label>
                                     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                                 </div>
                                 <div class="form-group">
-                                    <label for="exampleInputFile">File input</label>
-                                    <input type="file" id="exampleInputFile">
-                                    <p class="help-block">Example block-level help text here.</p>
+                                    <label for="exampleInputFile">Hiển Thị</label>
+                                    <select class="form-control input-sm m-bot15">
+                                        <option>Ẩn </option>
+                                        <option>Hiển Thị </option>
+                                    </select>
                                 </div>
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox"> Check me out
-                                    </label>
-                                </div>
-                                <button type="submit" class="btn btn-info">Submit</button>
+                                
+                                <button type="submit" name="add_category_product" class="btn btn-info">Thêm</button>
                             </form>
                             </div>
 
